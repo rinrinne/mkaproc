67ace1e7079fa2ec129a59d54129ff2fbf111bf4
Nobuhiro Hayashi <rinrin.ne@gmail.com>
2009-03-01 05:30:16 +0900

    setup script for win32 add

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+	MkaProc setup.
+
+	Copyright (c) 2006-2007 by Nyaochi
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+http://www.gnu.org/copyleft/gpl.html .
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(
+	name='mkaproc',
+	version='1.0',
+	description='Matroska Processor (MkaProc)',
+	author='Hahahaha',
+	author_email='rin_ne@big.or.jp',
+	url='http://www20.big.or.jp/~rin_ne/',
+	py_modules=[
+		"mkaproc",
+		"Matroska",
+		"textfile"
+		],
+	console=['mkaproc.py'],
+	options={"py2exe": {"packages": ["encodings"]}},
+	)
